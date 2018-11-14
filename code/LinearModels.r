90d984ae6fc78df3008d563cf6646835e9f46714
Jared Lander <info@jaredlander.com>
2018-11-14 12:39:36 -0500

    Showed multiple regression, fitted vs actual, and coefplot

--- code/LinearModels.r
+++ code/LinearModels.r
@@ -17,3 +17,26 @@ tip1
 0.92 + 0.105*15
 
 summary(tip1)
+
+
+head(tips)
+
+tip2 <- lm(tip ~ total_bill + size, data=tips)
+summary(tip2)
+
+0.668 + 0.092*30 + 0.192*1
+0.668 + 0.092*30 + 0.192*2
+0.668 + 0.092*45 + 0.192*2
+
+library(broom)
+
+tidy_tip2 <- tidy(tip2)
+head(tidy_tip2)
+aug_tip2 <- augment(tip2)
+head(aug_tip2)
+
+ggplot(aug_tip2, aes(x=tip, y=.fitted)) + geom_point() + 
+    geom_smooth() + geom_abline()
+
+summary(tip2)
+coefplot(tip2, sort='magnitude')
